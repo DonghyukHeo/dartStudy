40bfe84d7170f7d8b314b013b49d28cbc6a170f1
javadori <javadori@gmail.com>
2023-08-01 15:23:43 +0900

    mac에서 작성한 코드 체크

--- flutter_study/flutter_application_1/lib/main.dart
+++ flutter_study/flutter_application_1/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/javadori_home_page.dart';
+//import 'package:flutter_application_1/pages/javadori_home_page.dart';
 import 'package:flutter_application_1/pages/my_home_page.dart';
 
 /*
