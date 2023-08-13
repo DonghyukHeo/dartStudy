e5bc1deecb7d11bef65f947a75387d6b28c20bec
javadori <javadori@gmail.com>
2023-08-13 19:49:10 +0900

    커스텀 toast 스터디

--- flutter_study/flutter_package_study/lib/page/home_page.dart
+++ flutter_study/flutter_package_study/lib/page/home_page.dart
@@ -9,6 +9,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late FToast
+      fToast; // late 는 해당 변수가 none nullable일 경우 에러가 발생할 수 있으나 initState에서 바로 처리가 되므로 late 키워드를 사용한다.
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,15 +31,17 @@ class _HomePageState extends State<HomePage> {
           children: [
             ElevatedButton(
               onPressed: () {
-                Fluttertoast.showToast(
-                  msg: "허대장이 만든 Toast 메시지",
-                  toastLength: Toast.LENGTH_LONG,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.blue,
-                  textColor: Colors.white,
-                  fontSize: 20.0,
-                );
+                // Fluttertoast.showToast(
+                //   msg: "허대장이 만든 Toast 메시지",
+                //   toastLength: Toast.LENGTH_LONG,
+                //   gravity: ToastGravity.CENTER,
+                //   timeInSecForIosWeb: 1,
+                //   backgroundColor: Colors.blue,
+                //   textColor: Colors.white,
+                //   fontSize: 20.0,
+                // );
+
+                _showToast(); //build context 가 필요한 형태 호출
               },
               child: const Text('btn'),
             ),
@@ -38,5 +50,43 @@ class _HomePageState extends State<HomePage> {
             ),
           ]),
     );
+  }
+
+  void _showToast() {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.greenAccent,
+      ),
+      child: const Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.check),
+          SizedBox(
+            width: 12.0,
+          ),
+          Text("This is a Custom Toast"),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: const Duration(seconds: 5),
+    );
+
+    // Custom Toast Position
+    fToast.showToast(
+        child: toast,
+        toastDuration: const Duration(seconds: 2),
+        positionedToastBuilder: (context, child) {
+          return Positioned(
+            top: 16.0,
+            left: 16.0,
+            child: child,
+          );
+        });
   }
 }
