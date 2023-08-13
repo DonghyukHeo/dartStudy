a9fd56c44e8a97e73857c9bd6c25d2d388be2be5
javadori <javadori@gmail.com>
2023-08-13 19:50:47 +0900

    주석 수정

--- flutter_study/flutter_package_study/lib/main.dart
+++ flutter_study/flutter_package_study/lib/main.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_package_study/page/home_page.dart';
 
 /*
+  pub.dev
+
+  외부 라이브러리 패키지
+  패키지와 플러그인 : 플러그인은 패키지의 유형이다. 전체 명칭은 플러그인 패키지임.
+
+  사용하고자 하는 패키지를 검색하여 Readme에서 내용을 확인한다.
+  설치 방법을 체크하여 설치 한후 git에서 샘플을 코드 확인
+
   별도 패키지를 설치하여 설정할 경우
   pubspec.yaml 파일에 관련 내용을 작성하며, 해당 내용을 작성할 경우
   dependencies 부분에 작성해야 한다.
