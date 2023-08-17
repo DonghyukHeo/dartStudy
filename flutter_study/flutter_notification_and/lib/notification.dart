4eec748de1c2396bef3fb95acae022e5f807c405
Javadori <javadori@hotmail.com>
2023-08-17 09:06:49 +0900

    notification 안드로이드 스터디

---
+++ flutter_study/flutter_notification_and/lib/notification.dart
@@ -0,0 +1,64 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class FlutterLocalNotification {
+  FlutterLocalNotification._();
+
+  static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static init() async {
+    AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings('mipmap/ic_launcher');
+
+    DarwinInitializationSettings iosInitializationSettings =
+        const DarwinInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    );
+
+    InitializationSettings initializationSettings = InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iosInitializationSettings,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  static requestNotificationPermission() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+
+    /* 
+      안드로이드 13 (API level 33) 에서는 IOS와 같이 requestPermissions으로 권한을
+      받아야 알림이 정상적으로 동작한다.
+    */
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestPermission();
+  }
+
+  static Future<void> showNotification() async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channel id', 'channel name',
+            channelDescription: 'channel description',
+            importance: Importance.max,
+            priority: Priority.max,
+            showWhen: false);
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails,
+        iOS: DarwinNotificationDetails(badgeNumber: 1));
+
+    await flutterLocalNotificationsPlugin.show(
+        0, 'test title', 'test body', notificationDetails,
+        payload: 'item x');
+  }
+}
